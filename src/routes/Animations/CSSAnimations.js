6409a15a616637f68bbde28d956e52ec8444fe5d
janoo <janoo@simplymeasured.com>
2017-08-08 10:30:21 -0700

    CSSAnimations and stretchy input boxes

--- src/routes/Animations/CSSAnimations.js
+++ src/routes/Animations/CSSAnimations.js
@@ -9,16 +9,30 @@ import React from 'react';
 export default class CSSAnimations extends React.PureComponent {
   state = {
     animated: false,
+    focused: false,
   }
   animate = () => {
     this.setState((state) => ({
       animated: !state.animated,
     }))
   }
+  onFocus = () => {
+    this.setState(() => ({ focused: true }))
+  }
+  onBlur = () => {
+    this.setState(() => ({ focused: false }))
+  }
   render() {
     return (
       <div>
         <p>CSSAnimations</p>
+        <input
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          className={['fancy-input', this.state.focused && 'fancy-input-focused'].join(' ')}
+          placeholder="Stretch Me!"
+        />
+        <br/>
         <button
           onClick={this.animate}
         >Animate</button>
