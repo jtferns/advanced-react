83df5e6e585af6a7cd6d83cd327bfdd49fe82fbe
janoo <janoo@simplymeasured.com>
2017-08-08 13:46:35 -0700

    Basic higher order component

--- src/routes/HOC/BasicHOC.js
+++ src/routes/HOC/BasicHOC.js
@@ -3,9 +3,19 @@ import React from 'react'
 const Greeting = () =>
   <p>Hello World!</p>
 
-const greetingWithFunc = (func) => (Component) => (props) => {
-  func();
-  return <Component {...props} />;
+// const greetingWithFunc = (func) => (Component) => (props) => {
+//   func();
+//   return <Component {...props} />;
+// }
+
+// rewrite and return a Class
+const greetingWithFunc = (func) => (Component) => {
+  return class extends React.Component {
+    render() {
+      func();
+      return <Component {...this.props} />;
+    }
+  }
 }
 
 const log = () => console.log('hello from higher order component');
