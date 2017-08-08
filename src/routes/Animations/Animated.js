0f65d65662d9023ff1020764362c0cab1a896cb6
janoo <janoo@simplymeasured.com>
2017-08-08 11:44:50 -0700

    All the Animated animations

--- src/routes/Animations/Animated.js
+++ src/routes/Animations/Animated.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import Animated from 'animated/lib/targets/react-dom';
+import Easing from 'animated/lib/Easing';
+
+// Can import this directly in React Native
+// import { Animated } from 'react-native';
+
+// react-native-animatable has pre-wrapped animatable components
 
 /* Exercise:
 *  Use interpolation to create an animated message that will show up after a
@@ -10,36 +16,136 @@ import Animated from 'animated/lib/targets/react-dom';
 
 // use this.animatedValue.setValue(0) to reset a value
 
+const data = ['banana', 'pineapple', 'mango', 'kiwi'];
+
 export default class AnimatedExample extends React.PureComponent {
-  state = {
-    animated: false,
+  constructor() {
+    super()
+    this.state= {
+      animated: false,
+    }
+    // this.animatedValues = [];
+    // data.forEach((item) => {
+    //   this.animatedValues[item] = new Animated.Value(0)
+    // })
   }
 
+  // animatedValue = new Animated.Value(0);
+  // animatedMarginLeft = new Animated.Value(-430);
+  // animatedOpacity = new Animated.Value(0);
   animatedMarginLeft = new Animated.Value(0);
+  animatedOpacity = new Animated.Value(0);
+
+  // componentDidMount() {
+  //   this.animate()
+  // }
+
+  animate = () => {
+    this.animatedMarginLeft.setValue(0);
+    this.animatedOpacity.setValue(0);
+    // const animations = data.map((item, index) => (
+    //   Animated.timing(
+    //     this.animatedValues[item],
+    //     {
+    //       toValue: 1,
+    //       duration: 500,
+    //       delay: index * 100
+    //     }
+    //   )
+    // ));
+    // Animated.parallel(animations
+    Animated.parallel([
+      Animated.timing(this.animatedMarginLeft, { toValue: 1, duration: 800, delay: 100, easing: Easing.bounce }),
+      Animated.timing(this.animatedOpacity, { toValue: 1, duration: 800, delay: 100 })
+    ]).start()
+  }
 
   updateMargin = () => {
     Animated.timing(
-      this.animatedMarginLeft,
+      this.animatedValue,
       {
-        duration: 300,
-        toValue: this.state.animated ? 0 : 300,
+        duration: 1000,
+        toValue: 1,
+        easing: Easing.linear,
       }
-    ).start()
-    this.setState((state) => ({ animated: !state.animated }))
+    ).start(
+      // this takes a callback!
+      () => console.log('i did a thing')
+    )
+    // this.setState((state) => ({ animated: !state.animated }))
   }
 
 
   render() {
+    // const animatedMarginLeft = this.animatedValue.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: [0, 300],
+    // });
+    // const fontSize = this.animatedValue.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: [16, 32],
+    // });
+    // const backgroundColor = this.animatedValue.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: ['red', 'orange'],
+    // });
+
+    const animatedMarginLeft = this.animatedMarginLeft.interpolate({
+      inputRange: [0, 1],
+      outputRange: [250, 6],
+    });
 
     return (
       <div>
-        <p>Animated</p>
-        <button onClick={this.updateMargin}>Update Margin</button>
-        <Animated.div style={Object.assign({}, styles.box, { marginLeft: this.animatedMarginLeft })} />
+        <button onClick={this.animate} style={styles.button}>Upvote!</button>
+        <Animated.div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: 200,
+          border: 'solid 1 gray',
+          borderRadius: 4,
+          backgroundColor: '#eee',
+          marginTop: animatedMarginLeft,
+          opacity: this.animatedOpacity,
+          textAlign: 'center',
+        }}>
+          <p style={{ padding: 6 }}>⬆️</p>
+        </Animated.div>
       </div>
     )
   }
 }
+
+// <Animated.div style={Object.assign({}, styles.box, { marginLeft: this.animatedMarginLeft })} />
+
+// <p>Animated</p>
+// <button onClick={this.updateMargin}>Animate!</button>
+//
+// <Animated.div style={Object.assign({}, styles.box, { marginLeft: animatedMarginLeft })} />
+//
+// <Animated.div style={{ fontSize }}>
+//   Hello World!
+// </Animated.div>
+//
+// <Animated.div style={Object.assign({}, styles.box, { backgroundColor })} />
+
+// <Animated.div style={{ opacity: this.animatedOpacity }}>
+//   <p style={styles.welcome}>Welcome</p>
+// </Animated.div>
+// <Animated.div style={{
+//   display: 'flex', flexDirection: 'column', marginLeft: this.animatedMarginLeft, width: 400
+// }}>
+//   <input style={styles.input} />
+//   <button style={styles.button}>Sign Up</button>
+// </Animated.div>
+
+// {
+//   data.map((item, index) => {
+//     <Animated.div style={Object.assign({}, styles.box, { opacity: this.animatedValues[item] })}>
+//       { item }
+//     </Animated.div>
+//   })
+// }
 
 const styles = {
   box: {
@@ -49,4 +155,20 @@ const styles = {
     backgroundColor: 'red',
     transition: 'all .5s linear'
   },
+  welcome: {
+    fontSize: 25,
+  },
+  input: {
+    margin: 10,
+    backgroundColor: 'pink',
+    fontSize: 22,
+  },
+  button: {
+    backgroundColor: 'purple',
+    border: 'none',
+    outline: 'none',
+    fontSize: 18,
+    color: '#fff',
+    padding: 6
+  }
 }
